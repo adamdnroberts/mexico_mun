e21cbc2b61130a443eb4c3ea9c56a66cc663a510
adamdnroberts <70497853+adamdnroberts@users.noreply.github.com>
2025-03-03 08:34:29 -0500

    update code for different match specifications, rename merge_shape_files

--- code/make_matching_graphs2.28.25.R
+++ code/make_matching_graphs2.28.25.R
@@ -1,12 +1,8 @@
-minimal_match$match = "minimal"
-medium_match1$match = "medium1" 
-medium_match2$match = "medium2" 
-  full_match1$match = "full1"
-  full_match2$match = "full2"
+library(ggplot2)
 
-test <- rbind(minimal_match, medium_match1, medium_match2, full_match1, full_match2)
+matching_plot_data <- read.csv("~/mexico_mun/data/matching_plot_data.csv", header=T)
 
-matching_plot1 <- ggplot(subset(test, bw_type == "MSE" & match != "medium2" & match != "full2"), aes(x = n, y = est
+matching_plot1 <- ggplot(subset(matching_plot_data, bw_type == "MSE" & match != "medium2" & match != "full2"), aes(x = n, y = est
                                                 , color = match)) +
   geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2, position = position_dodge(width = -0.5)) +
   geom_point(position = position_dodge(width = -0.5)) +
@@ -21,7 +17,7 @@ print(matching_plot1)
 ggsave(filename = "C:/Users/adamd/Dropbox/Apps/Overleaf/Third Year Paper Results Outline/images/num_refs_different_matches1.png", plot = matching_plot1, width = 6, height = 4)
 
 
-matching_plot2 <- ggplot(subset(test, bw_type == "MSE" & match != "medium1" & match != "full1"), aes(x = n, y = est
+matching_plot2 <- ggplot(subset(matching_plot_data, bw_type == "MSE" & match != "minimal"), aes(x = n, y = est
                                                                                    , color = match
 )) +
   geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2, position = position_dodge(width = -0.5)) +
@@ -34,4 +30,4 @@ matching_plot2 <- ggplot(subset(test, bw_type == "MSE" & match != "medium1" & ma
 
 print(matching_plot2)
 
-ggsave(filename = "C:/Users/adamd/Dropbox/Apps/Overleaf/Third Year Paper Results Outline/images/num_refs_different_matches1.png", plot = matching_plot2, width = 6, height = 4)
+ggsave(filename = "C:/Users/adamd/Dropbox/Apps/Overleaf/Third Year Paper Results Outline/images/num_refs_different_matches2.png", plot = matching_plot2, width = 6, height = 4)
